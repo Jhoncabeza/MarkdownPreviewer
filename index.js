d48c8b81db77773e4b476a267d4cc04dbc000315
Jhon Cabeza <cabejhon7@gmail.com>
2022-01-16 20:44:40 -0500

    Update Markdown

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function App() {
     const [text, setText] = React.useState("");
 
     return (
-        <div className="text-center px-4 bg-[#F6F5F5] min-h-screen">
+        <div className="w-full text-center px-4 bg-[#F6F5F5] min-h-screen" >
             <h1 className="p-4 text-2xl text-[#1687A7]">MI MARKDOWN PREVIEWER</h1>
             <textarea
                 name="text"
@@ -17,9 +17,9 @@ function App() {
                 rows="6"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
-                className="border-2 border-[#276678] rounded-md shadow-lg bg-[#F6F5F5] textarea"
+                className="w-full min-h-[40vh] border-2 border-[#276678] rounded-md shadow-lg bg-[#F6F5F5] "
             ></textarea>
-            <h3 className="mt-3 text-2xl text-[#1687A7]">SALIDA</h3>
+            <h1 className="mt-3 text-2xl text-[#1687A7]">SALIDA</h1>
             <Preview markdown={text} />
         </div>
     );
@@ -33,8 +33,10 @@ function Preview({ markdown }) {
                 __html: marked(markdown, { renderer: renderer }),
             }}
             id="preview"
+            className="w-full border-2  min-h-[40vh] rounded-md border-[#276678]"
         >
+            
         </div>
     );
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
